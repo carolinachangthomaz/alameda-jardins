f4baaeae24fd1747734576e1e809527a1392791c
Carolina Thomaz <carol.com.jp@gmail.com>
2018-10-03 21:51:03 -0300

    Sobre

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { ProjetosComponent } from './components/projetos/projetos.component';
+import { SobreComponent } from './components/sobre/sobre.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavbarComponent,
-    ProjetosComponent
+    ProjetosComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule
