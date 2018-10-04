878dbf6c5bcaff1884a93bbc21c36410082f2702
Carolina Thomaz <carol.com.jp@gmail.com>
2018-10-03 21:48:30 -0300

    Projetos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProjetosComponent } from './components/projetos/projetos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    ProjetosComponent
   ],
   imports: [
     BrowserModule
